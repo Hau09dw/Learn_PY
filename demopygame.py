4d72dd7abc09022dbdc39d8ecdac54144c005919
F.R.I.D.A.Y <hauhm2323@gmail.com>
2023-12-15 17:55:42 +0700

    learnpygame

--- demopygame.py
+++ demopygame.py
@@ -1,26 +1,62 @@
-import pygame
-import random
+import pygame, sys 
+from pygame.locals import *
 
-WIDTH = 600
-HEIGHT = 600
-FPS = 30 # khung hinh tren giay
-
+WIDTH = 480
+HIGHT = 270
+FPS = 60
 
 pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+DISPLAYSURF = pygame.display.set_mode((WIDTH,HIGHT))
+pygame.display.set_caption("Animation")
+fpsclock = pygame.time.Clock()
 
-# Game loop
-running = True
-while running:
-    clock.tick(FPS)
+# font = pygame.font.SysFont('consolas',30)
+# txtsurface = font.render('Heloooooo', True,(0,255,0),(255,0,0))
+background = pygame.image.load("background.jpg")
+background = pygame.transform.scale(background,(WIDTH,HIGHT))
+class demo():
+    def __init__(self):
+        self.x = 0
+        self.y = 100
+        self.surface = pygame.image.load('demoanh.png')
+    def draw(self):
+        DISPLAYSURF.blit(self.surface,(self.x,self.y)) 
+        
+    def update(self,moveleft,moveright):
+        if moveleft == True:
+            self.x -= 2
+        if moveright == True:
+            self.x +=2
+        if self.x + 100 > WIDTH:
+            self.x = WIDTH - 100
+        if self.x < 0:
+            self.x = 0
+Demo = demo()
+moveleft = False
+moveright = False
+while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    # Du lieu tu nguoi choi
-    # Cap nhat thong so cua game
-    # Ve cac doi tuong ra man hinh game
-    pygame.display.flip()
-pygame.quit()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                moveleft = False
+                
+            if event.key == K_RIGHT:
+                moveright = False
+            
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                moveleft = True
+            if event.key == K_RIGHT:
+                moveright = True
+            
+    DISPLAYSURF.blit(background,(0,0))
+    # DISPLAYSURF.blit(txtsurface, (100,100))
+    # pygame.draw.line(DISPLAYSURF,(0,0,255),(0,0),(100,100),5)
+    Demo.draw()
+    Demo.update(moveleft,moveright)
+    pygame.display.update()
+    fpsclock.tick(FPS)
